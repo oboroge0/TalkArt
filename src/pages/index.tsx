02708b7e8c34a89dcb29415c271d369273e68844
Nikunikuo <yajusika@gmail.com>
2025-09-04 23:42:23 +0900

    Restore proper background image with buildUrl and correct CSS classes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ const Home = () => {
   const bgUrl =
     (webcamStatus || captureStatus) && useVideoAsBackground
       ? ''
-      : `url(/backgrounds/bg-test.svg)`
+      : `url(${buildUrl(backgroundImageUrl)})`
   const messageReceiverEnabled = settingsStore((s) => s.messageReceiverEnabled)
   const modelType = settingsStore((s) => s.modelType)
   const { t } = useTranslation()
@@ -93,7 +93,7 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="h-[100svh] bg-red-500" style={{ backgroundImage: bgUrl }}>
+    <div className="h-[100svh] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: bgUrl }}>
       <Meta />
       {!isTalkArtMode && <Introduction />}
       {modelType === 'vrm' ? <VrmViewer /> : <Live2DViewer />}
